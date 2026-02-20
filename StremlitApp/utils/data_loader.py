1ee5401261652532201908c6d27a9cebc91be5c8
Dhruvi Jhanjhri <dhruvijhanjhri@gmail.com>
2026-02-20 19:12:32 +0530

    Fixed loader function, file path, and dependencies

--- StremlitApp/utils/data_loader.py
+++ StremlitApp/utils/data_loader.py
@@ -1,11 +1,20 @@
 import os
 import pandas as pd
 
-def load_data():
-    base_dir = os.path.dirname(os.path.dirname(__file__))
+def load_forecast():
+    # Get root directory of StreamlitApp
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Build correct path
     file_path = os.path.join(base_dir, "data", "final_crypto_forecast_master.csv")
-    
+
+    # Debug print (optional but useful)
+    print("Looking for file at:", file_path)
+
+    # Read CSV
     df = pd.read_csv(file_path)
+
+    # Convert date column
     df["date"] = pd.to_datetime(df["date"])
-    
+
     return df
